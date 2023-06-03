8a05c12ca765ba4f15c1a052f9eb78dbd3b6c357
Nate Whetsell <nathan.whetsell@gmail.com>
2023-06-03 07:26:20 -0400

    Initial commit

---
+++ grammar.js
@@ -0,0 +1,28 @@
+const rules = require('./rules.js');
+
+module.exports = grammar({
+  name: 'lilypond_scheme',
+
+  rules: Object.assign(
+    {
+      scheme_program: $ => repeat(
+        $._scheme_token
+      )
+    },
+
+    rules,
+
+    {
+      scheme_embedded_lilypond: $ => seq(
+        '#{',
+        optional($.scheme_embedded_lilypond_text),
+        '#}'
+      ),
+
+      scheme_embedded_lilypond_text: $ => repeat1(choice(
+        /[^#]+/,
+        /#[^}]/
+      ))
+    }
+  )
+});
