fbcdf48f221ac67a775b082b0777a0fae389081f
Nate Whetsell <nathan.whetsell@gmail.com>
2024-01-21 12:51:45 -0500

    Add queries

---
+++ queries/update_builtins.py
@@ -0,0 +1,34 @@
+from pygments.regexopt import regex_opt
+import pygments.lexers._lilypond_builtins as _lilypond_builtins
+import pygments.lexers._scheme_builtins as _scheme_builtins
+import re
+from textwrap import dedent
+
+with open('highlights-builtins.scm', 'w') as file:
+    regex = re.sub(r'\\(.)', r'\\\\\1', regex_opt(_scheme_builtins.scheme_keywords, '^', '$'))
+    file.write(dedent(f'''\
+    (
+      (scheme_symbol) @keyword
+      (#match? @keyword "{regex}")
+    )
+    '''))
+
+    # Remove operator-like functions.
+    for item in ['*', '+', '-', '/', '<', '<=', '=', '>', '>=']:
+        _scheme_builtins.scheme_builtins.remove(item)
+    regex = re.sub(r'\\(.)', r'\\\\\1', regex_opt(_scheme_builtins.scheme_builtins, '^', '$'))
+    file.write(dedent(f'''\
+    (
+      (scheme_symbol) @identifier.core.function
+      (#match? @identifier.core.function "{regex}")
+    )
+    '''))
+
+with open('highlights-lilypond-builtins.scm', 'w') as file:
+    regex = re.sub(r'\\(.)', r'\\\\\1', regex_opt(_lilypond_builtins.scheme_functions, '^', '$'))
+    file.write(dedent(f'''\
+    (
+      (scheme_symbol) @identifier.core.function
+      (#match? @identifier.core.function "{regex}")
+    )
+    '''))
