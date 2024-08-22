a6a7f05715403e622f315232aa73adc8fd62dc06
Nate Whetsell <nathan.whetsell@gmail.com>
2024-08-22 07:03:07 -0400

    Use consistent quotations marks

--- queries/update_builtins.py
+++ queries/update_builtins.py
@@ -5,7 +5,7 @@ import re
 from textwrap import dedent
 
 with open('highlights-builtins.scm', 'w') as file:
-    regex = re.sub(r'\\(.)', r'\\\\\1', regex_opt(_scheme_builtins.scheme_keywords, '^', '$').replace("\\-", "-"))
+    regex = re.sub(r'\\(.)', r'\\\\\1', regex_opt(_scheme_builtins.scheme_keywords, '^', '$').replace('\\-', '-'))
     file.write(dedent(f'''\
     (
       (scheme_symbol) @keyword
@@ -16,7 +16,7 @@ with open('highlights-builtins.scm', 'w') as file:
     # Remove operator-like functions.
     for item in ['*', '+', '-', '/', '<', '<=', '=', '>', '>=']:
         _scheme_builtins.scheme_builtins.remove(item)
-    regex = re.sub(r'\\(.)', r'\\\\\1', regex_opt(_scheme_builtins.scheme_builtins, '^', '$').replace("\\-", "-"))
+    regex = re.sub(r'\\(.)', r'\\\\\1', regex_opt(_scheme_builtins.scheme_builtins, '^', '$').replace('\\-', '-'))
     file.write(dedent(f'''\
     (
       (scheme_symbol) @identifier.core.function
@@ -25,7 +25,7 @@ with open('highlights-builtins.scm', 'w') as file:
     '''))
 
 with open('highlights-lilypond-builtins.scm', 'w') as file:
-    regex = re.sub(r'\\(.)', r'\\\\\1', regex_opt(_lilypond_builtins.scheme_functions, '^', '$').replace("\\-", "-"))
+    regex = re.sub(r'\\(.)', r'\\\\\1', regex_opt(_lilypond_builtins.scheme_functions, '^', '$').replace('\\-', '-'))
     file.write(dedent(f'''\
     (
       (scheme_symbol) @identifier.core.function
